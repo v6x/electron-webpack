5dd272f49417da2e6d61b9e57af0f8a78a150045
jaeyoung.choi <lactrious@gmail.com>
2018-09-28 05:12:33 +0900

    Deploy source-map-option

---
+++ packages/electron-webpack/out/electron-main-hmr/HmrServer.d.ts
@@ -0,0 +1,9 @@
+import Crocket from "crocket";
+import { Stats } from "webpack";
+export declare class HmrServer {
+    private state;
+    readonly ipc: Crocket;
+    listen(): Promise<string>;
+    beforeCompile(): void;
+    built(stats: Stats): void;
+}
